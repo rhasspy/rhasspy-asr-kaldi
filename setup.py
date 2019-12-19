4a6f92daf74e3741706d9de853ae1411c91078a6
Michael Hansen <hansen.mike@gmail.com>
2019-12-19 10:58:15 -0500

    Add manifest, VERSION

--- setup.py
+++ setup.py
@@ -155,12 +155,15 @@ this_dir = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(this_dir, "README.md"), "r") as readme_file:
     long_description = readme_file.read()
 
-with open("requirements.txt", "r") as requirements_file:
+with open(os.path.join(this_dir, "requirements.txt"), "r") as requirements_file:
     requirements = requirements_file.read().splitlines()
+
+with open(os.path.join(this_dir, "VERSION"), "r") as version_file:
+    version = version_file.read().strip()
 
 setuptools.setup(
     name="rhasspy-asr-kaldi",
-    version="0.1",
+    version=version,
     author="Michael Hansen",
     author_email="hansen.mike@gmail.com",
     url="https://github.com/synesthesiam/rhasspy-asr-kaldi",
